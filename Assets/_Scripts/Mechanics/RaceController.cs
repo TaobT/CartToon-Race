98273081ab0b7866cd3ed7f9500207a032a67ec5
TaobT <ninjotase165@gmail.com>
2024-10-25 00:21:01 -0600

    sistema de vueltas

---
+++ Assets/_Scripts/Mechanics/RaceController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceController : MonoBehaviour
+{
+    public static RaceController instance;
+    [SerializeField] private int totalCheckpoints;
+
+    //Para un solo jugador (por ahora)
+    private int nextCheckpoint = 0;
+
+    public int NextCheckpoint => nextCheckpoint;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void CheckpointReached(int checkpointId)
+    {
+        if(checkpointId == nextCheckpoint)
+        {
+            nextCheckpoint++;
+            if(nextCheckpoint >= totalCheckpoints)
+            {
+                nextCheckpoint = 0;
+                Debug.Log("Vuelta Completada");
+            }
+        }
+    }
+}
